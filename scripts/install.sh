d2ca42ce6d5f279c2c1092996b69b4a4c22a25ba
Mete Durlu <mete.durlu@ibm.com>
2025-09-18 22:48:59 +0200

    scripts/install: Backup old configs during install
    Signed-off-by: Mete Durlu <mete.durlu@ibm.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,32 +1,39 @@
 #!/usr/bin/bash
 
+set -v
+
 ROOT_DIR=$PWD/../
+CP_ARGS="-b --suffix='.old'"
 
 function install_vim_config()
 {
-	cp $ROOT_DIR/vim/vimrc $HOME/.vimrc
+	cp $CP_ARGS $ROOT_DIR/vim/vimrc $HOME/.vimrc
 }
 
 function install_git_config()
 {
-	cp $ROOT_DIR/git/gitconfig $HOME/.gitconfig
+	cp $CP_ARGS $ROOT_DIR/git/gitconfig $HOME/.gitconfig
 }
 
 function install_alacritty_config()
 {
 	mkdir $HOME/.config/alacritty
-	cp $ROOT_DIR/alacritty/alacritty.toml $HOME/.config/alacritty/
+	cp $CP_ARGS $ROOT_DIR/alacritty/alacritty.toml $HOME/.config/alacritty/
 	mkdir -p $HOME/.config/alacritty/themes
-	git clone https://github.com/alacritty/alacritty-theme $HOME/.config/alacritty/themes
+	git clone https://github.com/alacritty/alacritty-theme \
+		$HOME/.config/alacritty/themes
 }
 
 function install_i3_config()
 {
-	cp $ROOT_DIR/i3/config $HOME/.config/i3/
+	cp $CP_ARGS $ROOT_DIR/i3/config $HOME/.config/i3/
 }
 
+echo "Start config install"
 install_vim_config
 install_git_config
 install_alacritty_config
 install_i3_config
+echo "Installation finished"
+echo "Old configs are backed up"
 
